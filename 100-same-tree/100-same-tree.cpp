3e3eb967f633e5ee57e36d7a3db814a033f2d58f
Selva T <drtselva2000@gmail.com>
2022-09-19 19:15:52 +0530

    Time: 4 ms (45.97%), Space: 10.2 MB (6.74%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -18,17 +18,17 @@ public:
         return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);*/
             
         TreeNode *left, *right;
-        if (!p or !q)
+        if (p == NULL or q == NULL)
             return p==q;
         
-        queue<TreeNode*> q1;
+        queue<TreeNode*> q1, q2;
         q1.push(p);
-        q1.push(q);
-        while(!q1.empty()){
+        q2.push(q);
+        while (!q1.empty() && !q2.empty()){
             left = q1.front();
             q1.pop();
-            right = q1.front();
-            q1.pop();
+            right = q2.front();
+            q2.pop();
             if (NULL == left && NULL == right)
                 continue;
             if (NULL == left || NULL == right)
@@ -36,10 +36,10 @@ public:
             if (left->val != right->val)
                 return false;
             q1.push(left->left);
-            q1.push(right->left);
+            q2.push(right->left);
             
             q1.push(left->right);
-            q1.push(right->right);
+            q2.push(right->right);
             
         }
         return true;
