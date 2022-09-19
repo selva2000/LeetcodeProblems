7a6ca6ad6a43c071d84e42970292217f03724a70
Selva T <drtselva2000@gmail.com>
2022-09-19 19:08:49 +0530

    Time: 2 ms (70.17%), Space: 10.1 MB (6.74%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -12,14 +12,30 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        
-        /*int left = isSameTree(p->left, q->left);
-        int right = isSameTree(p->right, q->right);*/
-        
-        if(p == NULL or q == NULL){
+        /*if(p == NULL or q == NULL){
             return (p==q);
         }
-        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);*/
+            
+       queue<TreeNode*> Q;
+        if(!p or !q) return p==q;       //If both roots are null
+        Q.push(p); Q.push(q);           //Push both the roots in the queue                  
+        while(!Q.empty())  {
+            TreeNode* left = Q.front(); Q.pop();                //Store one node in left and pop
+            TreeNode *right = Q.front(); Q.pop();               //Store other in right and pop
+            if(!left and !right) continue;                      //If both nodes are null -> continue
+            if(!left or !right) return false;                   //If one of them is null, simply return false
+            if(left->val != right->val) return false;           //If they are not equal, return false
+            
+            //Push left childs of both nodes
+            Q.push(left->left);                         
+            Q.push(right->left);
+
+            //Push right child of both nodes
+            Q.push(left->right);
+            Q.push(right->right);
+        }
+        return true;  
         
     }
 };
