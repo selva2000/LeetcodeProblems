64907b3ed15cf4c9a18127556fd1e848abdadc89
Selva T <drtselva2000@gmail.com>
2022-09-27 20:37:50 +0530

    Time: 17 ms (78.19%), Space: 11 MB (32.81%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -6,19 +6,26 @@ public:
         
         //sort(nums.begin(), nums.end());
         
-        for(int i=0; i<n; i++){
+        //O(N^2)
+        /*for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
                 if(nums[i] + nums[j] == target){
                 v.push_back(i);
                 v.push_back(j);
                 break;
                 //return v;
+            }                
             }
-                
+        }*/
+        
+        //Optimized oprach to O(N)
+       unordered_map<int, int> indices;
+        for (int i = 0; i < nums.size(); i++) {
+            if (indices.find(target - nums[i]) != indices.end()) {
+                return {indices[target - nums[i]], i};
             }
-            
-            
+            indices[nums[i]] = i;
         }
-        return v;    
+        return {};
     }
 };
