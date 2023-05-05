fd8ec3e6b754ea7eee718af0e8bbbdc7f172ffaf
Selva T <drtselva2000@gmail.com>
2023-05-05 23:09:25 +0530

    Added solution - LeetHub

---
+++ Bleak Numbers - GFG/bleak-numbers.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+	int countSetBits(int x)
+{
+    unsigned int count = 0;
+    while (x) {
+        x &= (x - 1);
+        count++;
+    }
+    return count;
+}
+ 
+// Returns true if n is Bleak
+int is_bleak(int n)
+{
+    // Check for all numbers 'x' smaller
+    // than n.  If x + countSetBits(x)
+    // becomes n, then n can't be Bleak
+    for (int x = 1; x < n; x++)
+        if (x + countSetBits(x) == n)
+            return 0;
+ 
+    return 1;
+}
+};
+
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	Solution ob;
+    	int ans = ob.is_bleak(n);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
